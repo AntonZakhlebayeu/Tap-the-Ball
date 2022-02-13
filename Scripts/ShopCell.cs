56f4f5ed280dc6f47350b8d59a1f169a7721307f
Anton <antonzakhlebayeu@gmail.com>
2022-02-14 02:29:35 +0300

    Update ShopCell.cs

--- Scripts/ShopCell.cs
+++ Scripts/ShopCell.cs
@@ -6,17 +6,32 @@ using UnityEngine.UI;
 public class ShopCell : MonoBehaviour
 {
 	[SerializeField] public Text _costField;
+	[SerializeField] public int cellIndex;
+	public static ShopCell Instance;
 
-	public void Render(IShopItem shopItem)
+	private void Awake()
 	{
-		if (!shopItem.IsBought) _costField.text = shopItem.Cost.ToString();
+		if (Instance == null)
+			Instance = this;
+	}
+
+	public void Render(IShopItem shopItem, int index)
+	{
+		if (!shopItem.IsBought && !shopItem.IsChosen) _costField.text = shopItem.Cost.ToString();
 		else if (shopItem.IsBought && !shopItem.IsChosen) _costField.text = "Bought";
-		else _costField.text = "Chosen";
+		else if (shopItem.IsBought && shopItem.IsChosen) _costField.text = "Chosen";
+		cellIndex = index;
 	}
 
 	public void OnClickShopCell()
 	{
-		Debug.Log("Works");
+		var shopItem = ShopItemWrapper(cellIndex);
+		Shop.BuyShopItem(shopItem, cellIndex);
+	}
+
+	public static AssetShopItem ShopItemWrapper(int index)
+	{
+		return Shop.Instance.ShopItems[index];
 	}
 
 }
